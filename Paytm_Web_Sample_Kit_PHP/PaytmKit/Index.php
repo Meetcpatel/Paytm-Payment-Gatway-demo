14311d970eee69b9fec691cd79066f6408314a68
Meet Patel <meetcpatel906@gmail.com>
2019-05-06 18:09:14 +0530

    index

---
+++ Paytm_Web_Sample_Kit_PHP/PaytmKit/Index.php
@@ -0,0 +1,14 @@
+
+
+<html>
+<head>
+<title>Merchant Check Out Page</title>
+<meta name="GENERATOR" content="Evrsoft First Page">
+</head>
+<body>
+	<h1>Merchant Check Out Page</h1>
+	<pre>
+    </pre>
+    
+</body>
+</html>
